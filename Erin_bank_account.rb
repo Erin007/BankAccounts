f7087e6451b493218b4ee514e516f89e37c4bf5e
Erin Bond <erin@erinebond.com>
2016-08-23 13:54:27 -0700

    ensure that amounts round to the nearest cent

--- Erin_bank_account.rb
+++ Erin_bank_account.rb
@@ -13,7 +13,7 @@ module Bank
       @owner = owner
 
       puts "Account initialized."
-      puts "Your id is: #{@id}.\nYour starting balance is $#{@initial_balance}."
+      puts "Hi #{owner.name}. \nYour id is: #{@id}.\nYour starting balance is $#{sprintf "%.2f", @initial_balance}."
     end#of initialize
 
     def assert_pos_initial_balance (initial_balance, msg="You must start with a positive initial balance.")
@@ -26,11 +26,11 @@ module Bank
 
     def withdraw (money_withdraw)
       if (balance - money_withdraw) < 0
-        puts "Sorry, you do not have enough money to withdraw $#{money_withdraw}."
+        puts "Sorry, you do not have enough money to withdraw $#{sprintf "%.2f",money_withdraw}."
         check_balance
         return balance
       else
-        puts "You withdrew $#{money_withdraw}"
+        puts "You withdrew $#{sprintf "%.2f", money_withdraw}"
         @balance = (balance - money_withdraw)
         check_balance
         return(balance - money_withdraw)
@@ -38,14 +38,14 @@ module Bank
     end#of withdraw
 
     def deposit (money_deposit)
-      puts "You deposited $#{money_deposit}"
+      puts "You deposited $#{sprintf "%.2f", money_deposit}"
       @balance = (balance + money_deposit)
       check_balance
       return(balance + money_deposit)
     end#of deposit
 
     def check_balance
-      puts "Your balance is: $#{@balance} "
+      puts "Your balance is: $#{sprintf "%.2f", @balance} "
       return @balance
     end#of check_balance
   end#of class
@@ -65,14 +65,14 @@ module Bank
 end#of module
 
 owner1 = Bank::Owner.new("Sally", "1234 Mystery St. Seattle, WA", "sally@email.com")
-account1 = Bank::Account.new(432422, 100, owner1.name)
+account1 = Bank::Account.new(432422, 100, owner1)
 account1.withdraw(20)
 
 puts "---------------------------------------------------------------------"
 owner2 = Bank::Owner.new("Walter", "1600 Pennsylvania Ave Washington D.C", "walter@whitehouse.com")
-account2 = Bank::Account.new(737688, 500, owner2.name)
-account2.withdraw(200)
-account2.deposit(1000)
+account2 = Bank::Account.new(737688, 500.98, owner2)
+account2.withdraw(1000)
+account2.deposit(60.76)
 
 #to randomize id numbers
 #(rand (111111..999999)
